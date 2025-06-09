1e89b857317cd12289616aafe10c012d91035ead
rodrisy <rodrigo.s.yuste@gmail.com>
2025-06-09 12:30:04 -0400

    hello

---
+++ teddy/teddy/teddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  teddyApp.swift
+//  teddy
+//
+//  Created by Rodrigo Sánchez Yuste on 9/6/25.
+//
+
+import SwiftUI
+
+@main
+struct teddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ teddy/teddyTests/teddyTests.swift
@@ -0,0 +1,16 @@
+//
+//  teddyTests.swift
+//  teddyTests
+//
+//  Created by Rodrigo Sánchez Yuste on 9/6/25.
+//
+
+import Testing
+
+struct teddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
